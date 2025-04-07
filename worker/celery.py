1e75a4514faa3ae51bdad432b6a8068a4bdc9152
Holldix <antonfedoseenkov@gmail.com>
2025-04-07 14:04:53 +0300

    Update celery.py

--- worker/celery.py
+++ worker/celery.py
@@ -1,9 +1,20 @@
 from celery import Celery
 
-
-app = Celery("work",
-             broker="redis://redis:6379/10",
-             include=["worker.tasks"])
+app = Celery(
+    "worker",
+    broker="redis://redis:6379/10",
+    backend=None,
+    task_ignore_result=True,
+    broker_transport_options={
+        'visibility_timeout': 3600,
+        'fanout_prefix': True,
+        'fanout_patterns': True
+    },
+    task_routes={
+        'worker.tasks.send_signal': {'queue': 'high_priority'},
+        'worker.tasks.*': {'queue': 'default'}
+    }
+)
 
 if __name__ == "__main__":
     app.start()
